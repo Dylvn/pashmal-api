e962a9dc46e6b1d8f4802d8e5d57756849a28e70
Dylvn <trochain.dylan@gmail.com>
2020-08-13 17:21:21 +0200

    Adding migrations of database

---
+++ database/migrations/2020_08_13_150540_create_foreign_keys.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_genre', function (Blueprint $table) {
+            $table->foreignId('book_id')->constrained('books');
+            $table->foreignId('genres_id')->constrained('genres');
+        });
+
+        Schema::create('book_order', function (Blueprint $table) {
+            $table->foreignId('book_id')->constrained('books');
+            $table->foreignId('order_id')->constrained('orders');
+        });
+
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_genre');
+        Schema::dropIfExists('book_order');
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn(['user_id']);
+        });
+    }
+}
